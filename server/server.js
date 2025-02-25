fda2abeb02fd20cf66b5d28958c129adbfd8a208
vaishnavi-ambadkar <vaishnaviambadkar183@gmail.com>
2025-02-25 11:55:55 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://quiz-m-frontend-git-main-vaishnavi-ambadkars-projects.vercel.app", // Your frontend URL
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 mongoose
