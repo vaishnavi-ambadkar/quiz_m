4f23930a3e26c25fb51bfc5dfecc32a6cf8dad0f
vaishnavi-ambadkar <vaishnaviambadkar183@gmail.com>
2025-02-25 11:41:55 +0530

    Update Quiz.jsx

--- client/src/components/Quiz.jsx
+++ client/src/components/Quiz.jsx
@@ -11,7 +11,7 @@ export default function Quiz() {
   useEffect(() => {
     async function fetchQuestions() {
       try {
-        const response = await fetch("http://localhost:5000/questions");
+        const response = await fetch("https://quiz-m-api.vercel.app/questions");
         if (!response.ok) {
           throw new Error("Failed to fetch questions");
         }
